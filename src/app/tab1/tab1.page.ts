1abaf70afbc144e0e0cb3a0f11d0b62cf8fcdbf3
PC-CHECO\Sergio <sergio@mecatronium.com>
2022-03-05 15:44:19 -0600

    Avance practica 4

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -9,4 +9,52 @@ export class Tab1Page {
 
   constructor() {}
 
+  alumnos = [
+    {
+      "nombre": "Abraham",
+      "apellido": "Ramirez",
+      "matricula": "ABC123"
+    },
+    {
+      "nombre": "Sergio",
+      "apellido": "Gutierrez",
+      "matricula": "ABC123"
+    },
+    {
+      "nombre": "Diego",
+      "apellido": "Jasso",
+      "matricula": "ABC123"
+    },
+    {
+      "nombre": "Brian",
+      "apellido": "Esquivel",
+      "matricula": "ABC123"
+    },
+    {
+      "nombre": "Omar",
+      "apellido": "Garza",
+      "matricula": "ABC123"
+    },
+    {
+      "nombre": "Emiliano",
+      "apellido": "Hernandez",
+      "matricula": "ABC123"
+    }
+  ]
+
+  @Input() nombre: string = "";
+  @Input() apellido: string = "";
+  @Input() matricula: string = "";
+  
+
+
+  agregarAlumno(): void {
+     var  nuevoAlumno: any = {
+       "nombre": this.nombre,
+       "apellido": this.apellido,
+       "matricula": this.matricula
+     }
+
+     this.alumnos.push(nuevoAlumno)
+  }
 }
